33227ea8f696026a9a9c8bc8c3b1fa9ee427b9aa
candvert <leiyue159@gmail.com>
2025-09-20 11:03:36 +0800

    complete SSG

--- src/app/[dir]/[file]/page.tsx
+++ src/app/[dir]/[file]/page.tsx
@@ -9,6 +9,7 @@ import { read } from "to-vfile";
 import { unified } from "unified";
 import rehypeAddH2Class from "@/utils/md-styles";
 import path from "path";
+import { filesName } from "@/utils/get-mdx-filenames";
 
 const prettyCodeOptions: Options = {
   theme: "github-dark",
@@ -29,12 +30,29 @@ async function ok(filename: string) {
   return String(filecontent);
 }
 
+export async function generateStaticParams() {
+  const allDirsAndFiles = filesName();
+  
+  // 生成所有参数组合
+  const params = allDirsAndFiles.flatMap((dirArray) => {
+    const dirName = dirArray[0]; // 第一个元素是目录名
+    const fileNames = dirArray.slice(1); // 剩余元素是文件名
+    
+    return fileNames.map((fileName) => ({
+      dir: dirName,    // 匹配 [dir] 段
+      file: fileName   // 匹配 [file] 段
+    }));
+  });
+
+  return params;
+}
+
 export default async function Home({
   params,
 }: {
-  params: Promise<{ dir: string; file: string }>;
+  params: { dir: string; file: string };
 }) {
-  const { dir, file } = await params;
+  const { dir, file } = params;
   const decodedDir = decodeURIComponent(dir);
   const decodedFile = decodeURIComponent(file);
 
